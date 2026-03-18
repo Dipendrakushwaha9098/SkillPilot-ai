6cb9e70266fb8132fa67341bbc3694c936d51a99
Dipendra Kushwaha <dipendrakushwaha2846@gmail.com>
2026-03-18 23:40:32 +0530

    Add progress update and retrieval routes

--- backend/routes/progressRoutes.js
+++ backend/routes/progressRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const { updateProgress, getProgress } = require('../controllers/progressController');
 const { protect } = require('../middleware/authMiddleware');
 
+
 router.post('/update', protect, updateProgress);
 router.get('/', protect, getProgress);
+
 
 module.exports = router;

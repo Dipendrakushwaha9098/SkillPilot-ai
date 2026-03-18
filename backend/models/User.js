2f7501ad3a890dd7f17a1f82ee72c6fbc86d105e
Dipendra Kushwaha <dipendrakushwaha2846@gmail.com>
2026-03-18 23:46:52 +0530

    Add password hashing and comparison methods

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -20,11 +21,13 @@ const userSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now }
 });
 
+
 // Hash password before saving
 userSchema.pre('save', async function() {
   if (!this.isModified('password')) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
+
 
 // Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
